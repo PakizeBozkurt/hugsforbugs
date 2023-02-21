47e2c1aa70059a4f77d1a37bff67d8760a0bb275
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2023-02-21 03:12:04 +0000

    Added useState to the form to record any input from the input fields

--- client/src/components/FormPage.js
+++ client/src/components/FormPage.js
@@ -1,9 +1,13 @@
-import React from 'react'; 
+import { useState } from 'react'; 
 import { Form } from 'react-bootstrap'; 
 import  Button  from 'react-bootstrap/Button';
 
 
 const FormPage = () => {
+    const [FirstName, setFirstName] = useState(''); 
+    const [SurName, setSurName] = useState('');
+    const [Email, setEmail] = useState(''); 
+    const [SlackAccount, setSlackAccount] = useState('');
   return (
 		<main className={'container'}>
 			<div className={"row"}>
@@ -11,27 +15,53 @@ const FormPage = () => {
                         <Form>
                             <Form.Group className="mb-3">
                                 <Form.Label>First Name: </Form.Label>
-                                <Form.Control type="email" placeholder="First Name..."/>
+                                <Form.Control 
+                                type="email"
+                                 placeholder="First Name..." 
+                                 required
+                                 value = {FirstName}
+                                 onChange = {((e) => setFirstName(e.target.value))}
+                                 />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Surname: </Form.Label>
-                                <Form.Control type="email" placeholder="Surname..." />
+                                <Form.Control 
+                                type="email" 
+                                placeholder="Surname..." 
+                                required
+                                value = {SurName}
+                                onChange = {((e) => setSurName(e.target.value))}
+
+                                />
                             </Form.Group>
                             <Form.Group className="mb-3">
-                                <Form.Label>Send Email</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Your Email..." />
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control 
+                                type="email" 
+                                placeholder="Enter Your Email..." 
+                                required
+                                value={Email}
+                                onChange = {((e) => setEmail(e.target.value))}
+                                />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Slack Account </Form.Label>
-                                <Form.Control type ="text" placeholder ="Enter your slack username..." />
+                                <Form.Control 
+                                type ="text"
+                                placeholder ="Enter your slack username..." 
+                                required
+                                value = {SlackAccount}
+                                onChange = {((e) => setSlackAccount(e.target.value))}
+                                />
                             </Form.Group>
                             <Form.Group className="mb-3">
                                 <Form.Label>Availability</Form.Label>
-                                <Form.Control type="text" placeholder = "Enter your availability..."/>
+                                <Form.Control type="text" placeholder = "Enter your availability..." required/>
                             </Form.Group>
                             <Button variant = "primary" type = "submit"> 
                             Submit 
                             </Button>
+                            <p>{SlackAccount}</p>
                             </Form>                     
                     </div>
             </div> 
