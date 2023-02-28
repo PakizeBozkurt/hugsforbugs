2ad99e6d9dc52237bf2072729c975857c7b016b4
Pakize Bozkurt <pakiozihak@gmail.com>
2023-02-28 22:02:38 +0000

    Add user registration endpoint with input validation and error handling

--- server/authEndpoint.js
+++ server/authEndpoint.js
@@ -42,6 +42,15 @@ app.post(
 		check("name", "Please enter a name").isLength({ min: 1 }),
 	],
 	async (req, res) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, DELETE, OPTIONS"
+		);
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+		);
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
@@ -49,28 +58,25 @@ app.post(
 
 		const { email, password, name } = req.body;
 
-		try {
-			const salt = await bcrypt.genSalt(10);
-			const hashedPassword = await bcrypt.hash(password, salt);
-			const result = await pool.query(
-				"SELECT * FROM trainees WHERE email = $1",
-				[email]
-			);
-			if (result.rows.length > 0) {
-				return res
-					.status(400)
-					.json({ errors: [{ msg: "User already exists" }] });
-			}
-			const query =
-				"INSERT INTO trainees (email, password, name) VALUES ($1, $2, $3)";
-			const values = [email, hashedPassword, name];
-			await pool.query(query, values);
-			res.status(201).json({ msg: "User created" });
-		} catch (err) {
-			//eslint-disable-next-line
-			console.error(err);
-			res.status(500).json({ errors: [{ msg: "Server error" }] });
+		// Hash the password using bcrypt
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		// Check if user already exists in the database
+		const result = await pool.query("SELECT * FROM trainees WHERE email = $1", [
+			email,
+		]);
+		if (result.rows.length > 0) {
+			return res.status(400).json({ errors: [{ msg: "User already exists" }] });
 		}
+
+		// Insert new user into the database
+		const query =
+			"INSERT INTO trainees (email, password, name) VALUES ($1, $2, $3)";
+		const values = [email, hashedPassword, name];
+		await pool.query(query, values);
+
+		res.status(201).json({ msg: "User created" });
 	}
 );
 
