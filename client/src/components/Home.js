3e27c350dd458bba22819044439864a07ffcd4f5
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2023-03-04 21:49:59 +0000

    more styles and code refactoring

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,7 @@ import vid from "./pexels-c-technical-6334253.mp4";
 import "./home.css";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import Headings from "./Heading";
-import Subheading from "./Subheading";
+
 
 function Home() {
   return (
@@ -15,22 +14,16 @@ function Home() {
           <h1 className="header">Study Buddies</h1>
           <h4>Boost Your Productivity</h4>
           <p>
-            Check availability and connect with friends and other CYF trainees.
-          </p>
-          <p>Study together or join study groups</p>
-          <p>Make collaboration easier and more productive</p>
+            Check availability and connect with friends and other CYF trainees. Study together or join study groups. Make collaboration easier and more productive</p>
         </div>
         <div className="btn-wrapper">
-          <div>
+        
           <button className="login">
             <Link to={"/login"}>Login</Link>
           </button>
-        </div>
-        <div>
           <Button className="register">
             <Link to={"/register"}>Register</Link>
           </Button>
-        </div>
         </div>
         <video className="video" autoPlay loop muted>
         <source src={vid} type="video/mp4" />
