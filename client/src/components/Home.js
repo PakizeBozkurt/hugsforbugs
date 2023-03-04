53495fe0658e9ff40f00c2d3bd4322e6a0b653df
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2023-03-04 20:51:28 +0000

    some styles added to text and video

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,26 +10,33 @@ import Subheading from "./Subheading";
 function Home() {
   return (
     <div>
-    <div className = "text">
-      <h1>Study Buddies</h1>
-      <h4>Boost Your Productivity</h4>
-      <p>Check availability and connect with friends and other CYF trainees.</p>
-      <p>Study together or join study groups</p>
-      <p>Make collaboration easier and more productive</p>
-    </div>
-			<div>
-      <button>
-        <Link to={"/login"}>Login</Link>
-      </button>
-			</div>
-			<span>
-			<Button>
-        <Link to={"/register"}>Register</Link>
-      </Button>{" "}
-			</span>
-      <video className="video" autoPlay loop muted>
+      <div className="showcase">
+        <div className="text">
+          <h1 className="header">Study Buddies</h1>
+          <h4>Boost Your Productivity</h4>
+          <p>
+            Check availability and connect with friends and other CYF trainees.
+          </p>
+          <p>Study together or join study groups</p>
+          <p>Make collaboration easier and more productive</p>
+        </div>
+        <div className="btn-wrapper">
+          <div>
+          <button className="login">
+            <Link to={"/login"}>Login</Link>
+          </button>
+        </div>
+        <div>
+          <Button className="register">
+            <Link to={"/register"}>Register</Link>
+          </Button>
+        </div>
+        </div>
+        <video className="video" autoPlay loop muted>
         <source src={vid} type="video/mp4" />
       </video>
+      <div className="overlay"></div>
+      </div>
     </div>
   );
 }
