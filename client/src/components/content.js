cdbadd153509e9c71ebd3796d656f29e53399e43
Pakize Bozkurt <pakiozihak@gmail.com>
2023-03-05 22:18:03 +0000

    Added content.js file to refactoring

---
+++ client/src/components/content.js
@@ -0,0 +1,16 @@
+const content = {
+  home: {
+    title: "Study Buddies",
+    description: "Find study partners with matching availability.",
+  },
+  allUsers: {
+    title: "Trainees with matching availability",
+    searchPlaceholder: "Search",
+    dailyBtn: "Daily",
+    weeklyBtn: "Weekly",
+    monthlyBtn: "Monthly",
+    emailBtn: "Notify by Email",
+  },
+};
+
+export default content;
