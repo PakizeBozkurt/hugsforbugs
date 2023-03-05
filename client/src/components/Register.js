1bea32ebad52f0f985c88fd65fa77d4b48f8a1b3
micheale2021 <andemariammicheale@gmail.com>
2023-03-05 19:57:03 +0000

    add UI validation to register form and refactor code

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
 import Footer from "./Footer/Footer";
+import "../../src/register.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 const eye = <FontAwesomeIcon icon={faEye} />;
 
 const Register = () => {
+  const [validated, setValidated] = React.useState(false);
   const [name, setFirstName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
@@ -26,13 +30,20 @@ const Register = () => {
   const togglePasswordVisiblity = () => {
     setPasswordShown(passwordShown ? false : true);
   };
-  const register = (e) => {
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
     e.preventDefault();
     const user = {
       name,
       email,
       password,
     };
+
     fetch("https://study-buddies.onrender.com/register", {
       // this API comes from render.com
       method: "POST",
@@ -59,6 +70,7 @@ const Register = () => {
         }
       })
       .catch((err) => console.log(err));
+    setValidated(true);
   };
   const handleBack = (e) => {
     e.preventDefault();
@@ -66,66 +78,90 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 mt-5 mx-auto">
-            <h5>{errorMessage}</h5>
-            <Form style={{ width: "50%", margin: "auto" }}>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label style={{ color: "goldenrod" }}>
-                  First Name
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter First Name"
-                  name="firstName"
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label style={{ color: "goldenrod" }}>
-                  Email address
-                </Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  name="email"
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label style={{ color: "goldenrod" }}>
-                  Password <i onClick={togglePasswordVisiblity}>{eye}</i>
-                </Form.Label>
-                <Form.Control
-                  type={passwordShown ? "text" : "password"}
-                  placeholder="Password"
-                  name="password"
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Button
-                variant="outline-success"
-                style={{ margin: "10px" }}
-                type="submit"
-                onClick={register}
-              >
-                Register
-              </Button>
-              <Button
-                variant="outline-success"
-                style={{ margin: "10px" }}
-                type="submit"
-                onClick={handleBack}
-              >
-                Back
-              </Button>
-            </Form>
-            <Footer />
-          </div>
-        </div>
-      </div>
+    <div className="container row col-md-6 mt-5 mx-auto">
+      <h2 style={{ fontSize: "2.2em" }} className="text-center text-light">
+        REGISTER{" "}
+      </h2>
+
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleChange}
+        style={{ width: "50%", margin: "auto" }}
+      >
+        <h3 className="mb-2 text-danger font-italic">{errorMessage}</h3>
+        <Form.Group as={Col} className="mb-4" controlId="formBasicEmail">
+          <Form.Label className="text-light">First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+            onChange={handleChange}
+            name="firstName"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} className="mb-4" controlId="formBasicEmail">
+          <Form.Label className="text-light">Email</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Form.Control
+              type="email"
+              placeholder="email"
+              aria-describedby="inputGroupPrepend"
+              onChange={handleChange}
+              name="email"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid email.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group
+          as={Col}
+          className="mb-4"
+          md="6"
+          controlId="validationCustom05"
+        >
+          <Form.Label className="text-light">
+            Password <i onClick={togglePasswordVisiblity}>{eye}</i>
+          </Form.Label>
+          <Form.Control
+            type={passwordShown ? "text" : "password"}
+            placeholder="password"
+            onChange={handleChange}
+            name="password"
+            minLength={6}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Check
+            required
+            label="Agree to terms and conditions"
+            feedback="You must agree before submitting."
+            feedbackType="invalid"
+          />
+        </Form.Group>
+        <Button type="submit" onClick={handleSubmit}>
+          Submit form
+        </Button>
+        <Button
+          variant="outline-light"
+          style={{ margin: "10px" }}
+          type="submit"
+          onClick={handleBack}
+        >
+          Back
+        </Button>
+      </Form>
     </div>
   );
 };
