acd81532662b178e478070a429402b4cc347e377
micheale2021 <andemariammicheale@gmail.com>
2023-03-06 13:21:21 +0000

    resolve some conflict

--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -7,35 +7,22 @@ import Login from "./Login";
 import AvailableTrainees from "./AvailableTrainees";
 import UserProfile from "./UserProfile";
 import MyAvailability from "./MyAvailability";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import AllUsers from "./AllUsers";
-=======
->>>>>>> f0992b5 (Myavailability component added)
-=======
-import AllUsers from "./AllUsers";
->>>>>>> e5904a5 (NavBar and related components added)
+
 function Navigator() {
   return (
     <div>
-      <Routes>       
-        <Route path="/*" element={<Home />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/home" element={<AvailableTrainees />}/>
-        <Route path="/createavailability" element={<CreateAvailability />}/>
-        <Route path="/profile" element={<UserProfile />}/>
-        <Route path="/myavailability" element={<MyAvailability />}/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Route path="/allusers" element={<AllUsers />}/>
-        
-=======
->>>>>>> f0992b5 (Myavailability component added)
-=======
-        <Route path="/allusers" element={<AllUsers />}/>
-        
->>>>>>> e5904a5 (NavBar and related components added)
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<AvailableTrainees />} />
+        <Route path="/createavailability" element={<CreateAvailability />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/myavailability" element={<MyAvailability />} />
+
+        <Route path="/allusers" element={<AllUsers />} />
       </Routes>
     </div>
   );
