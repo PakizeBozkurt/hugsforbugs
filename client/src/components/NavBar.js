ec5b5b25c808e85cea121a5ba30326c715e8ecbd
micheale2021 <andemariammicheale@gmail.com>
2023-03-06 13:02:20 +0000

    format code

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,25 +4,34 @@ import UserInfoMenu from "./UserInfoMenu";
 function NavBar() {
   return (
     <div>
-      <Navbar bg="dark" variant="dark" expand="lg">
-    <Container>
-        <Navbar.Brand ><Image src="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png" alt="Code Your Future" width="100" height="50" />
+      <Container>
+        <Navbar.Brand>
+          <Image
+            src="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png"
+            alt="Code Your Future"
+            width="100"
+            height="50"
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" />
+        <Nav className="me-auto"></Nav>
+        <Nav.Link href="/allusers">All Users</Nav.Link>
+        <Navbar.Brand href="/home">SB</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link href="/allusers">All Users</Nav.Link>
+          <Nav className="me-auto">
+            <Nav.Link href="/all">All Users</Nav.Link>
             <Nav.Link href="/myavailability">My Availabilities</Nav.Link>
             <Nav.Link href="/createavailability">Create Availability</Nav.Link>
-            </Nav>
-            <Nav className="ml-auto">
-                <UserInfoMenu />
-            </Nav>
+          </Nav>
+          <Nav className="ml-auto">
+            <UserInfoMenu />
+          </Nav>
         </Navbar.Collapse>
-    </Container>
-</Navbar>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
