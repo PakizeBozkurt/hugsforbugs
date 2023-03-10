cc6150f74307aa09427fed963bc23e36d0853bae
micheale2021 <andemariammicheale@gmail.com>
2023-03-10 01:59:08 +0000

    refactor and change text size inside cards

--- client/src/components/AvailabilityCards.js
+++ client/src/components/AvailabilityCards.js
@@ -2,21 +2,15 @@ import React from "react";
 import SendEmail from "./SendEmail";
 function AvailibityCards(props) {
   return (
-    <div>
-      <div>
-        <div className="container">
-          <div className="card-body">
-            <h2 className="card-text">Name: {props.trainee.name}</h2>
-            <h2 className="card-text">
-              Date:{" "}
-              {new Date(props.trainee.availability_date).toLocaleDateString()}
-            </h2>
-            <h2 className="card-text">Topic: {props.trainee.topic}</h2>
-            <h2 className="card-text">
-              NOTIFY BY EMAIL <SendEmail trainee={props.trainee} />
-            </h2>
-          </div>
-        </div>
+    <div className="container">
+      <div className="card-body">
+        <h2 className="card-text">Name: {props.trainee.name}</h2>
+        <h5 className="card-text">
+          Date: {new Date(props.trainee.availability_date).toLocaleDateString()}
+        </h5>
+        <p className="card-text">Topic: {props.trainee.topic}</p>
+
+        <SendEmail trainee={props.trainee} />
       </div>
     </div>
   );
