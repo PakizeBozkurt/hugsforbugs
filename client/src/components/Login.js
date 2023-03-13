5bc152945c3d229ff3acf3698c78e54986b3c0f3
micheale2021 <andemariammicheale@gmail.com>
2023-03-13 16:36:07 +0000

    change route to my-availability

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -57,7 +57,7 @@ const Login = () => {
           localStorage.setItem("email", data.email);
           localStorage.setItem("name", data.name);
           localStorage.setItem("id", data.id);
-          window.location.href = "/createavailability";
+          window.location.href = "/my-availability";
         } else {
           alert("Wrong email or password");
           window.location.reload();
