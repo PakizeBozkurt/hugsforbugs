74571865be92cb3cd458a0c4aaf70b8747865fcc
micheale2021 <andemariammicheale@gmail.com>
2023-03-13 16:34:30 +0000

    Create new component

---
+++ client/src/components/MyAvailabilityCards.js
@@ -0,0 +1,30 @@
+import React from "react";
+// import DeleteMyAvailability from "./DeleteMyAvailability";
+// import EditMyAvailability from "./EditMyAvailability";
+
+function MyAvailabilityCards(props) {
+  return (
+    <div>
+      <div>
+        <div className={"card"}>
+          <div className={"card-body"}>
+            <h5 className="card-title">
+              {" "}
+              Date:{" "}
+              {new Date(props.date.availability_date).toLocaleDateString()}
+            </h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              Topic: {props.date.topic}
+            </h6>
+          </div>
+          <div>
+            {/* <DeleteMyAvailability id={props.date.id} />
+            <EditMyAvailability id={props.date.id} /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MyAvailabilityCards;
