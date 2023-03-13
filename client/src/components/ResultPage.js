33fda1ca631f327ca43188219bc2e2be540c4fff
micheale2021 <andemariammicheale@gmail.com>
2023-03-13 00:04:28 +0000

    remove handleBack & handleAll & update code.

--- client/src/components/ResultPage.js
+++ client/src/components/ResultPage.js
@@ -1,28 +1,65 @@
-import React from 'react';
+import React from "react";
 import Button from "react-bootstrap/Button";
+import Heading from "./Heading";
 
 const ResultPage = (props) => {
- const handleBack=(e)=>{
-     e.preventDefault();
-     window.location.href = '/createavailability';
- }
- const handleAll=(e)=>{
-        e.preventDefault();
-        props.filter("monthly")
- }
-    return (
-        <div>
-          <h1>No Result!</h1>
-            <p>
-                There is no available trainee for this date. Please try another date.
-            </p>
-            <div>
-                <Button onClick={handleBack} variant="primary" type="submit">
-                    Back
-                </Button>
+  return (
+    <div>
+      <Heading name={"List of Trainees"} />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search"
+              value={props.search}
+              onChange={props.handleSearch}
+            />
+          </div>
+          <div className="col-md-6">
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button
+                type="button"
+                className={`btn btn-${
+                  props.selected === "daily" ? "info" : "outline-info"
+                }`}
+                value="daily"
+                onClick={props.handleFilter}
+              >
+                Daily
+              </button>
+              <button
+                type="button"
+                className={`btn btn-${
+                  props.selected === "weekly" ? "info" : "outline-info"
+                }`}
+                value="weekly"
+                onClick={props.handleFilter}
+              >
+                Weekly
+              </button>
+              <button
+                type="button"
+                className={`btn btn-${
+                  props.selected === "monthly" ? "info" : "outline-info"
+                }`}
+                value="monthly"
+                onClick={props.handleFilter}
+              >
+                Monthly
+              </button>
             </div>
+          </div>
         </div>
-    );
+      </div>
+      
+      <h4 className="noResultHeading">
+        There is no available trainee for this date. Please check weekly and
+        monthly availability.
+      </h4>
+    </div>
+  );
 };
 
 export default ResultPage;
